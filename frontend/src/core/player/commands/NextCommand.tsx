9a80347dc8b9b938fabec8dc2eb3007bb11f51a7
Nguyễn Ngọc Trinh Nghi <523h0061@student.tdtu.edu.vn>
2026-02-12 19:58:51 +0700

    Feat: Clean frontend due to wrong configuration

--- frontend/src/core/player/commands/NextCommand.tsx
+++
@@ -1,8 +0,0 @@
-import type { Command } from "./Command";
-import { player } from "../Player";
-
-export class NextCommand implements Command {
-  execute(): void {
-    player.next();
-  }
-}
--- frontend/src/pages/home/SectionDetail.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const SectionDetail = () => {
-  return <div>SectionDetail</div>;
-};
-
-export default SectionDetail;
