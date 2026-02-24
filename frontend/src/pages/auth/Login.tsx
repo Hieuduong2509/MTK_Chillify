6f3de86ac4b0325ed666997cf2c9eead7b09d519
Võ Trọng Phát <523h0075@student.tdtu.edu.vn>
2026-02-24 20:04:43 +0700

    Feat: Finish UI of Login and Sign up page

--- frontend/src/pages/auth/Login.tsx
+++ frontend/src/pages/auth/Login.tsx
@@ -1,26 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { assets } from "../../assets/assets";
 
 const Login = () => {
   return (
     <div className="bg-background-dark min-h-screen flex items-center justify-center font-display antialiased p-4">
       <div className="w-full max-w-[440px] flex flex-col items-center">
         {/* Card Container */}
-        <div className="w-full bg-[#1C3340] rounded-xl shadow-[0_8px_30px_rgb(0,0,0,0.4)] border border-white/5 p-8">
-          
+        <div className="w-full bg-[#1C3340] rounded-xl shadow-[0_8px_30px_rgb(0,0,0,0.4)] border border-white/5 p-7">
           {/* Logo & Header */}
-          <div className="mb-10 flex flex-col items-center gap-3">
-            <div className="flex items-center justify-center w-14 h-14 bg-primary rounded-xl text-white shadow-lg shadow-primary/20">
-              <span className="material-symbols-outlined text-4xl">music_note</span>
+          <div className="flex flex-col items-center gap-3">
+            <div className="flex items-center justify-center w-44 h-44 object-contain">
+              <img src={assets.logo2} alt="Chillify's logo" className="" />
             </div>
-            <h2 className="text-white text-2xl font-bold tracking-tight">
-              Music Stream
-            </h2>
           </div>
 
           {/* Form */}
           <form className="space-y-5" onSubmit={(e) => e.preventDefault()}>
-            
             {/* Email Input */}
             <div className="flex flex-col gap-2">
               <label className="text-slate-200 text-sm font-semibold leading-normal">
@@ -56,17 +52,17 @@ const Login = () => {
             </div>
 
             {/* Actions */}
-            <div className="pt-4 space-y-6">
-              <button className="flex w-full cursor-pointer items-center justify-center overflow-hidden rounded-lg h-12 px-5 bg-primary hover:bg-[#1088e8] text-white text-base font-bold leading-normal tracking-[0.015em] transition-colors shadow-md shadow-primary/10">
+            <div className="pt-3 space-y-5">
+              <button className="flex w-full cursor-pointer items-center justify-center overflow-hidden rounded-lg h-12 px-5 bg-primary hover:bg-hover text-white text-base font-bold leading-normal tracking-[0.015em] transition-colors shadow-md shadow-primary/10">
                 <span className="truncate">Login</span>
               </button>
-              
+
               <div className="text-center">
                 <p className="text-slate-400 text-sm font-medium">
                   Don't have an account?
                   {/* Link chuyển sang trang Sign up */}
                   <Link
-                    className="text-primary hover:text-[#1088e8] font-bold ml-1 transition-colors"
+                    className="text-primary hover:text-hover font-bold ml-1 transition-colors"
                     to="/signup"
                   >
                     Sign up
